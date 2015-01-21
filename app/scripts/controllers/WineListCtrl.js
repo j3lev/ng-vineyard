c1091cf7041605529704ff700f1be74828f371f6
Joseph Levenson <josephlevenson@Dev-JLevenson.percussion.local>
2015-01-21 13:03:43 -0500

    modal without cross controller communication implemented

--- app/scripts/controllers/WineListCtrl.js
+++ app/scripts/controllers/WineListCtrl.js
@@ -1,30 +1,32 @@
 'use strict';
-
+'use strict';
 
 angular.module('ngVineyardApp')
-	.controller('WineListCtrl', function($scope, $modal, WineJSONsvc) {
-		
-		$scope.wines = [];
-		$scope.wineIndex = null;
-		$scope.editValues = [];
-		
-		WineJSONsvc.query(function(res){
-			$scope.wines = res;
-		}, function(err){
-			console.log(err);
-		});
-		
-		$scope.makeSelected = function(index, name, color, abv, type, vintage) {
-			$scope.editName = name;
-			$scope.editColor = color;
-			$scope.editABV = abv;
-			$scope.editType = type;
-			$scope.editVintage = vintage;
-			$scope.wineIndex = index;
-		}
+    .controller('WineListCtrl', function($scope, $modal, WineJSONsvc) {
 
-		$scope.sendIt = function(name, color, abv, type, vintage) {
-			$scope.wines[$scope.wineIndex].Name = name;
-		}
+        $scope.wines = [];
+        $scope.wineIndex = null;
+
+        WineJSONsvc.query(function(res) {
+            $scope.wines = res;
+        }, function(err) {
+            console.log(err);
+        });
+
+
+        $scope.edit = function(index, wine) {
+
+	        var editInstance = $modal.open({
+	            templateUrl: 'views/partials/modalEditWine.html',
+	            controller: 'editCtrl',
+	            size: 'sm',
+	            resolve: {
+	                
+	            }
+	        });
+	    };
+	})
+
+	.controller('editCtrl', function($scope, wine, index) {
 
 	});
