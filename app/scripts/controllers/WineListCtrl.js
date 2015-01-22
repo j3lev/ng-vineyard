0c7eb04b69c6df4754b28d5c3de2c7ab70c0f3e7
Joseph Levenson <josephlevenson@Dev-JLevenson.percussion.local>
2015-01-21 19:11:35 -0500

    Edit functionality fully implemented

--- app/scripts/controllers/WineListCtrl.js
+++ app/scripts/controllers/WineListCtrl.js
@@ -1,5 +1,4 @@
 'use strict';
-'use strict';
 
 angular.module('ngVineyardApp')
     .controller('WineListCtrl', function($scope, $modal, WineJSONsvc) {
@@ -13,20 +12,73 @@ angular.module('ngVineyardApp')
             console.log(err);
         });
 
+        $scope.edit = function(index, wine, isNew) {
 
-        $scope.edit = function(index, wine) {
+            var editInstance = $modal.open({
+                templateUrl: 'views/partials/modalEditWine.html',
+                controller: 'editCtrl',
+                size: 'md',
+                resolve: {
+                    index: function() {
+                        return index;
+                    },
+                    wine: function() {
+                        return wine;
+                    },
+                    isNew: function() {
+                        return isNew;
+                    }
+                }
+            });
 
-	        var editInstance = $modal.open({
-	            templateUrl: 'views/partials/modalEditWine.html',
-	            controller: 'editCtrl',
-	            size: 'sm',
-	            resolve: {
-	                
-	            }
-	        });
-	    };
-	})
+            editInstance.result.then(function(editedWine) {
+                update(editedWine, index);
+            });
 
-	.controller('editCtrl', function($scope, wine, index) {
+        };
 
-	});
+        function update(wine, index) {
+            $scope.wines[index] = wine;
+        }
+
+    })
+
+.controller('editCtrl', function($scope, $modalInstance, wine, index, isNew) {
+
+        if (isNew) {
+            $scope.CreateOrEdit = "Create New";
+            $scope.wine = {
+                Name: "",
+                Type: "",
+                Color: "red",
+                Vintage: "",
+                ABV: "",
+                Characteristics: {
+                    Acidity: "",
+                    Sweet: "",
+                    Fruit: "",
+                    Body: ""
+                }
+            };
+        } else {
+            $scope.CreateOrEdit = "Edit";
+            $scope.wine = angular.copy(wine);
+        }
+        $scope.wine.Color = $scope.wine.Color.toLowerCase();
+
+
+        $scope.editSubmit = function() {
+            if (isNew) {
+                console.log($scope.wine);
+            }
+            $modalInstance.close($scope.wine);
+        }
+
+
+
+
+
+
+
+
+});
